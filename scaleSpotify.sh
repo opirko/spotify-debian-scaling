17458b3f4a53f9a5bce10ddb47a841ccd7f5820a
Ondrej Pirko <ondra@pirko.cz>
2021-12-03 00:23:08 +0100

    more robust grepping and replacing

--- scaleSpotify.sh
+++ scaleSpotify.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-# this script scales spotify
+# this script scales spotify, customize config file path and scaling factor below.
 
 #SPOTCONF_PATH="/usr/share/applications/spotify.desktop"
 SPOTCONF_PATH="./spotify.desktop"
 SPOT_SCALE="2"
 
-echo "Following string is located in spotify config path:"
-sudo grep 'spotify %U' ${SPOTCONF_PATH}
-echo "This will be replaced with  spotify --force-device-scale-factor=${SPOT_SCALE} %U"
+SEARCH_STRING="spotify %U"
+GREPOUT=`sudo grep "${SEARCH_STRING}" ${SPOTCONF_PATH}`
+if [ -z "$GREPOUT" ]
+then
+	echo "String ${SEARCH_STRING} wasn't found in the given file."
+	SEARCH_STRING="spotify --force-device-scale-factor=. %U" 
+	GREPOUT=`sudo grep "${SEARCH_STRING}" ${SPOTCONF_PATH}`
+	if [ -z "$GREPOUT" ]
+	then
+		echo "String ${SEARCH_STRING} wasn't found in the given file, terminating."
+		exit 1
+	fi
+fi
+# string in some form was found
+echo "String ${SEARCH_STRING} was found in the given file."
+echo "This will be replaced with spotify --force-device-scale-factor=${SPOT_SCALE} %U"
 read -p "Is that ok? Type y or Y for confirmation." -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	sudo sed -i "s/spotify %U/spotify --force-device-scale-factor=${SPOT_SCALE} %U/" ${SPOTCONF_PATH}
+	sudo sed -i "s/${SEARCH_STRING}$/spotify --force-device-scale-factor=${SPOT_SCALE} %U/" ${SPOTCONF_PATH}
 	echo "Operation executed."
+	exit 0
+else
+	echo "Operation not executed."
+	exit 0
 fi
